831036d1c9c324c603d95911d6588d6e0ee5944c
Akashdeep Kashyap <akashdeepk619@gmail.com>
2022-01-31 00:43:35 +0530

    Google BtnChanges1

--- src/app/services/googleAuthService/google-auth.service.ts
+++ src/app/services/googleAuthService/google-auth.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { error } from 'protractor';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleAuthService {
-
+  
   constructor(private http:HttpClient) { }
 
   checkExistingUser(auth_token:any):Observable<any>{
-    const url="http://localhost:3000/socializeAPI/v1.0/userManagement/googlesAuth/checkExistingUser"
+    const url=environment.port + "/socializeAPI/v1.0/userManagement/googlesAuth/checkExistingUser"
     return this.http.post<any>(url,{auth_token:auth_token})
   }
 }
--- src/app/sign-in-sign-up/sign-in-sign-up.component.ts
+++ src/app/sign-in-sign-up/sign-in-sign-up.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit, NgZone } from "@angular/core";
 import { Router } from "@angular/router";
 import { DataSharingServiceService } from "../services/dataSharing/data-sharing-service.service";
-
+import { environment } from "src/environments/environment";
 import { GoogleAuthService } from "../services/googleAuthService/google-auth.service";
 
 declare const gapi: any;
 @Component({
   selector: "app-sign-in-sign-up",
   templateUrl: "./sign-in-sign-up.component.html",
-  styleUrls: ["./sign-in-sign-up.component.css"]
+  styleUrls: ["./sign-in-sign-up.component.css"],
 })
 export class SignInSignUpComponent implements OnInit {
   container: Element;
-  issignup:boolean
-  islogin:boolean;
-  myLocalStorage=window.localStorage
+  issignup: boolean;
+  islogin: boolean;
+  myLocalStorage = window.localStorage;
   existingUser: Boolean;
+
   ngOnInit() {
+    document.getElementById("googleBtn").style.display="none";
     this.googleInit();
     this.islogin = false;
     this.issignup = false;
+ 
   }
 
   constructor(
@@ -29,49 +32,53 @@ export class SignInSignUpComponent implements OnInit {
   ) {}
 
   //for google sign-in
-  private clientId: string =
-    "587767058687-f3677th4t0m5om1873i5p4vm4vf39ppf.apps.googleusercontent.com";
+  private clientId: string = environment.googleClientID;
+
   public auth2: any;
   profile: any;
   public showUsernameForm = false;
   private token: any;
   error: any;
-  private scope = [
-    "profile",
-    "email"
-  ].join(" ");
+  private scope = ["profile", "email"].join(" ");
 
-  public async googleInit() {
-    gapi.load("auth2", async () => {
-      this.auth2 = gapi.auth2.init({
-        client_id: this.clientId,
-        cookiepolicy: "single_host_origin",
-        scope: this.scope
-      });
-      this.attachSignin(document.getElementById("googleBtn"));
+  public googleInit() {
+    gapi.load("auth2", () => {
+      gapi.auth2
+        .init({
+          client_id: this.clientId,
+          cookiepolicy: "single_host_origin",
+          scope: this.scope,
+        })
+        .then((authInstance) => {
+          this.auth2 = authInstance;
+          // this.isgoogleInit = true;
+          this.attachSignin(document.getElementById("googleBtn"));
+        });
     });
   }
 
   public async attachSignin(element) {
     try {
+     element.style.display="block";
       this.auth2.attachClickHandler(
         element,
         {},
-        googleUser => {
+        (googleUser) => {
           element.innerHTML =
             "Signed in: " + googleUser.getBasicProfile().getName();
           this.profile = googleUser.getBasicProfile();
           this.token = googleUser.getAuthResponse().id_token;
           //store data in local storage
-          this.myLocalStorage.setItem("gProfile",JSON.stringify(this.profile))
-          this.myLocalStorage.setItem("token",this.token)
-          this.myLocalStorage.setItem("userAuthProvider","googleAuth")
+          this.myLocalStorage.setItem("gProfile", JSON.stringify(this.profile));
+          this.myLocalStorage.setItem("token", this.token);
+          this.myLocalStorage.setItem("userAuthProvider", "googleAuth");
           // console.log(this.profile);
           this.googleAuthService.checkExistingUser(this.token).subscribe(
-            res => {
+            (res) => {
               if (res.error) {
                 throw new Error(res.error);
               }
+          
               this.existingUser = res;
               //check weather user exists in db
               if (true) {
@@ -81,27 +88,25 @@ export class SignInSignUpComponent implements OnInit {
                     state: {
                       profile: this.profile,
                       userAuthProvider: "googleAuth",
-                      token: googleUser.getAuthResponse().id_token
-                    }
+                      token: googleUser.getAuthResponse().id_token,
+                    },
                   });
                 });
               }
-              if(this.existingUser==true){
+              if (this.existingUser == true) {
                 //redirect to home component
                 // this.router.navigate("/")
               }
-              console.log(this.existingUser);
+              // console.log(this.existingUser);
             },
-            err => {
-              this.error=err.error.error
-              window.location.reload();
-              console.log(this.error)
+            (err) => {
+              this.error = err.error.error;
+              // window.location.reload();
+              console.log(this.error);
             }
           );
-
-        
         },
-        function(error) {
+        function (error) {
           this.error = error.message;
         }
       );
@@ -112,22 +117,21 @@ export class SignInSignUpComponent implements OnInit {
 
   public signOut() {
     let auth = gapi.auth2.getAuthInstance();
-    auth.signOut().then(function() {
+    auth.signOut().then(function () {
       console.log("User signed out.");
     });
   }
-/**Google sign in ends */
+  /**Google sign in ends */
 
+  activateSlidingWindowClass(event: Event) {
+    const tar = event.target as Element;
 
-  activateSlidingWindowClass(event:Event) {
-    const tar = event.target as Element
-   
     /**If signup btn clicked than display signup form else display login  */
-    if(tar.id == "signup") {
+    if (tar.id == "signup") {
       console.log(tar.id);
       this.islogin = false;
       this.issignup = true;
-    } else if(tar.id == "login") {
+    } else if (tar.id == "login") {
       console.log(tar.id);
       this.islogin = true;
       this.issignup = false;
