4cf34717d8059b925286f35aa9e073989771a8c0
Akashdeep Kashyap <akashdeepk619@gmail.com>
2022-01-30 22:39:46 +0530

    Pre Deploy-1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true
-  
+  production: true,
+  port:"https://apisocialize.herokuapp.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  port:"http://localhost:3000"
+  port:"https://apisocialize.herokuapp.com"
 };
 
 /*
