0e71dc85bd013c268ccadd71df34f94ccecea028
AdkxKashyap <akashdeepk619@gmail.com>
2022-01-31 12:12:21 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   port:"https://apisocialize.herokuapp.com",
-  googleClientID:"587767058687-f3677th4t0m5om1873i5p4vm4vf39ppf.apps.googleusercontent.com"
+  googleClientID:
 };
